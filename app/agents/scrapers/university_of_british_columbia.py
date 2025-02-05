35c520a1eafa5bef344d4b8a683f8248d7b8f28a
Kathy <jikathycho@gmail.com>
2025-02-04 19:58:28 -0500

    fix: ubc scraper format

--- app/agents/scrapers/university_of_british_columbia.py
+++ app/agents/scrapers/university_of_british_columbia.py
@@ -4,26 +4,30 @@ import json
 from playwright.async_api import async_playwright
 import logging
 
+import pytz
+
 from .base import BaseScraper
-from app.models.course import CourseListingModel
+from app.models.course import (CourseListingModel, Day)
 
 
 class UniversityOfBritishColumbiaScraper(BaseScraper):
     def __init__(self, task_name: str, logger: logging.Logger):
         super().__init__(task_name, logger)
         self.base_url = "https://courses.students.ubc.ca/browse-courses"
-        self.data = []
+        self.totalPrograms = 0
+        self.totalCourses = 0
+        self.totalSections = 0
 
     def _determine_term(self) -> str:
         """Determine current term based on date"""
         month = datetime.now().month
         
         if 1 <= month <= 4:
-            return "Winter"
+            return "Winter 2"
         elif 5 <= month <= 8:
             return "Summer"
         else:  # 9 <= month <= 12
-            return "Winter"  # UBC uses Winter for Fall term
+            return "Winter 1"  # UBC uses Winter for Fall term
         return "SampleTerm"
     
     async def setupPage(self, page, pageNum):
@@ -47,12 +51,40 @@ class UniversityOfBritishColumbiaScraper(BaseScraper):
         for x in range(pageNum):
             await page.wait_for_selector('#subjects-table')
             await page.locator('#pagination-next').click()
+    
+    async def setDays(self, days_str):
+        if days_str == '':
+            return None
+        day_map = {
+            'Monday': Day.MONDAY,
+            'Tuesday': Day.TUESDAY,
+            'Wednesday': Day.WEDNESDAY,
+            'Thursday': Day.THURSDAY,
+            'Friday': Day.FRIDAY,
+            'Saturday': Day.SATURDAY,
+            'Sunday': Day.SUNDAY
+        }
+        days_str = days_str.split(', ')
+        days = []
+        for day in days_str:
+            if day in day_map:
+                days.append(day_map[day].value)
+        return days
+
+    async def setTime(self, date, time):
+        pst_tz = pytz.timezone('America/Vancouver')
+        if await time.inner_html() == '':
+            return None
+        converted_date = datetime.strptime(await date.inner_html(), '%Y-%m-%d').date()
+        converted_time = datetime.strptime(await time.inner_html(), '%H:%M').time()
+        dt = datetime.combine(converted_date, converted_time)
+        dt_tz = pst_tz.localize(dt)
+        return int(dt_tz.timestamp())
 
     async def fetch_courses(self) -> CourseListingModel:
         r = []
-        stop = False
         len = 0
-        window = 3
+        window = 4
 
         async with async_playwright() as p:
             browser = await p.chromium.launch()
@@ -62,33 +94,61 @@ class UniversityOfBritishColumbiaScraper(BaseScraper):
                 "height": 800,
             })
             await page.goto(self.base_url)
-        
-            while not stop:
-                await self.setupPage(page, len)
-                await page.wait_for_selector('tfoot')
-                await page.locator('#pagination-next').click()
-                if await page.locator('#pagination-next').is_disabled():
-                    stop = True
-                    break
-                len += 1
+            await page.wait_for_selector('#subjects-table')
+            pagination = (await page.locator('.MuiTablePagination-displayedRows').inner_html()).split(' of')
+            subjectLen = int(pagination[0].split('-')[1].strip())
+            totalSubjectLen = int(pagination[1].strip())
+            len = int(totalSubjectLen / subjectLen) + 1
+            itr = len / window
+            remainder = len % window
+
+            r = await asyncio.gather(*([asyncio.ensure_future(self.fetch_courses_e(page=page, pageNum=u)) for u in range(0,len)]))
+
+            # for i in range(window, len + window, window):
+            #     r.append(await asyncio.gather(*([asyncio.ensure_future(self.fetch_courses_e(page=page, pageNum=u)) for u in range(i-window,i)])))
             
-            for i in range(window, len + window, window):
-                r.append(await asyncio.gather(*([asyncio.ensure_future(self.fetch_courses_e(pageNum=u)) for u in range(i-window,i)])))
-
-
-    async def fetch_courses_e(self, pageNum) -> CourseListingModel:
-        """Fetch course data from UBC API"""
+            # for i in range(remainder):
+            #     self.fetch_courses_e(page=page, pageNum=i+(window * itr))
+            await browser.close()
+        
         year = datetime.now().year
         term = self._determine_term()
         
-        self.logger.info(f"=== Start UBC scraper for {term} {year} in page {pageNum}===")
+        data = {
+            "semester": f"{term} {year}",
+            "programs": r,
+            "total_programs": self.totalPrograms,
+            "total_courses": self.totalCourses,
+            "total_sections": self.totalSections
+        }
+
+        with open('data.json', 'w') as f:
+            json.dump(data, f)
+
+        # for d in r:
+        #     data['semester'] = d['semester']
+        #     data['programs'].extend(d['programs'])
+        #     data['total_programs'] += d['total_programs']
+        #     data['total_courses'] += d['total_courses']
+        #     data['total_sections'] += d['total_sections']
+        
+        return data
+
+
+    async def fetch_courses_e(self, page, pageNum) -> CourseListingModel:
+        """Fetch course data from UBC API"""
+        year = datetime.now().year
+        term = self._determine_term()
+        lecture_types = ['Lecture', 'Seminar', 'Independent Study', 'Thesis']
+        
+        self.logger.info(f"=== Start UBC scraper for {term} {year} in page {pageNum + 1} ===")
         
         # retrieve each html page for courses from ubc website
         # https://github.com/KogoCampus/course-scraper-job/blob/dff5418512899a57492d697a596f9a6ab477ebf9/scrapers/ubc/ubcScraper.py
 
         # parse each html page into json using self.llm_html_parser
-        async with async_playwright() as p:
-            browser = await p.chromium.launch()
+        async with async_playwright() as p2:
+            browser = await p2.chromium.launch()
             page = await browser.new_page()
             await page.set_viewport_size({
                 "width": 500,
@@ -106,14 +166,10 @@ class UniversityOfBritishColumbiaScraper(BaseScraper):
 
             prevName = ""
 
-            i = 0
-
-            if i != 0:
-                await page.wait_for_selector('main')
-                await page.locator('#pagination-next').click()
-
-            self.logger.info(f'Page {i+1}')
             subjects = await page.locator('td[data-colindex="0"] a').all()
+
+            res = []
+            self.totalPrograms += len(subjects)
 
             for j in range(0, len(subjects), 1):
                 currentName = await subjects[j].inner_text()
@@ -145,9 +201,15 @@ class UniversityOfBritishColumbiaScraper(BaseScraper):
                     }
 
                     courses = await page.locator('td[data-colindex="0"] a').all()
+                    self.totalCourses += len(courses)
                     for k in courses:
-                        await k.click()
-                        await page.wait_for_selector('.course-view')
+                        try:
+                            await k.click()
+                            await page.wait_for_selector('.course-view')
+                        except:
+                            self.logger.info(f"A section in {currentName} failed")
+                            continue
+
                         currentCourse = {
                             "courseName": "",
                             "courseCode": "",
@@ -164,81 +226,80 @@ class UniversityOfBritishColumbiaScraper(BaseScraper):
                         if html:
                             sessions = await page.locator('.course-sections-box').all()
                             sessionIndex = 0
-                            for s in sessions:
-                                session = await s.inner_html()
-                                if sessionIndex == 0:
-                                    prompts = [
-                                        "Parse this HTML section into a list of course sessions. Each session should have:",
-                                        "- sessionName: The section code/name (e.g., 'L1A', 'LAB 2', etc.)",
-                                        "- sessionType: The activity of the section (e.g., 'Lecture' or 'Seminar' or 'Thesis' or 'Independent Study', etc)"
-                                        "- campus: This value must always be 'Vancouver'",
-                                        "- location: The room/building location (null if not available)",
-                                        "- schedules: A list of schedule objects, each containing:",
-                                        "  - days: List of days as ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']",
-                                        "  - startTime: Unix timestamp for start time (null if not available)",
-                                        "  - endTime: Unix timestamp for end time (null if not available)",
-                                        "- childSession: An empty list"
+                            self.totalSections += len(sessions)
+                            for s in range(1, len(sessions)+1):
+                                data = await page.locator(f'.course-sections-box:nth-of-type({s}) p').all()
+                                
+                                converted_days = await self.setDays(await data[8].inner_html())
+                                converted_startTime = await self.setTime(data[6], data[7])
+                                converted_endTime = await self.setTime(data[9], data[10])
+                                if await data[4].inner_html() in lecture_types:
+                                    out = [
+                                        {
+                                            'sessionName': await data[0].text_content(),
+                                            'sessionType': await data[4].inner_html(),
+                                            'campus': 'Vancouver',
+                                            'location': None,
+                                            'schedules': [
+                                                {
+                                                    'days': converted_days,
+                                                    'startTime': converted_startTime,
+                                                    'endTime': converted_endTime
+                                                }
+                                            ],
+                                            'childSession': []
+                                        }
                                     ]
                                 else:
-                                    prompts = [
-                                        "Parse this HTML section into a list of course sessions. If sessionType is 'Lecture' or 'Seminar' or 'Thesis' or 'Independent Study', each session should have:",
-                                        "- sessionName: The section code/name (e.g., 'L1A', 'LAB 2', etc.)",
-                                        "- sessionType: The activity of the section (e.g., 'Lecture' or 'Seminar' or 'Thesis' or 'Independent Study')"
-                                        "- campus: This value must always be 'Vancouver'",
-                                        "- location: The room/building location (null if not available)",
-                                        "- schedules: A list of schedule objects, each containing:",
-                                        "  - days: List of days as ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']",
-                                        "  - startTime: Unix timestamp for start time (null if not available)",
-                                        "  - endTime: Unix timestamp for end time (null if not available)",
-                                        "- childSession: An empty list"
-                                        "If sessionType is other than 'Lecture' or 'Seminar' or 'Thesis' or 'Independent Study',",
-                                        "change'sessionName' to 'childSessionName' and 'sessionType' must be 'childSessionType'",
-                                        "and no need for childSession",
+                                    out = [
+                                        {
+                                            'childSessionName': await data[0].text_content(),
+                                            'childSessionType': await data[4].inner_html(),
+                                            'campus': 'Vancouver',
+                                            'location': None,
+                                            'schedules': [
+                                                {
+                                                    'days': converted_days,
+                                                    'startTime': converted_startTime,
+                                                    'endTime': converted_endTime
+                                                }
+                                            ]
+                                        }
                                     ]
-                                out = await self.llm_html_parser.parse_html_to_json_list(session, prompts)
-                                self.logger.info(out)
+
                                 if "childSessionName" in out[0]:
                                     for s in currentCourse.get('sessions'):
                                         if "sessionName" in s:
-                                            self.logger.info(s)
                                             s['childSession'].extend(out)
                                 else:
                                     currentCourse['sessions'].extend(out)
                                 sessionIndex += 1
-
                         currentProgram['courses'].append(currentCourse)
                         await page.go_back()
                         courses = await page.locator('td[data-colindex="0"] a').all()
                     
                     await page.go_back()
-                    self.data.append(currentProgram)
+                    res.append(currentProgram)
                     await page.wait_for_selector('td[data-colindex="0"] a')
                     isFirstPage = await page.evaluate("""
                         async () => {
                             return document.querySelector('td[data-colindex="0"] a').textContent
                         }
                     """)
-                    if isFirstPage != firstName and i != 0:
+                    if isFirstPage != firstName and pageNum != 0:
                         await page.goto(self.base_url)
-                        await self.setupPage(page, i)
+                        await self.setupPage(page, pageNum)
                     
                     await page.wait_for_selector('td[data-colindex="0"] a')
                     subjects = await page.locator('td[data-colindex="0"] a').all()
-            
-            i += 1
-            pageNumber = (await (await page.query_selector('tfoot p')).inner_text()).split('-')[1].split(' ')
-            if pageNumber[0] == pageNumber[2]:
-                stop = True
-            courseName = await (await page.query_selector('.l-node__title')).inner_text()
+
+                pageNumber = (await (await page.query_selector('tfoot p')).inner_text()).split('-')[1].split(' ')
+                if pageNumber[0] == pageNumber[2]:
+                    stop = True
+                courseName = await (await page.query_selector('.l-node__title')).inner_text()
 
             await browser.close()
 
         # return data in foramt of models/course.py
 
-        return {
-            "semester": f"{term} {year}",
-            "programs": self.data,
-            "total_programs": 0,
-            "total_courses": 0,
-            "total_sections": 0
-        } 
+        return res
